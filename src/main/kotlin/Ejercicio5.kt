61680cd3dee20a2487c6960fcb40506ef2959a05
Óscar Luque Porca <oscarluqueproca@gmail.com>
2024-09-18 16:27:07 +0200

    KOTLIN INTRO COMENTADO

--- src/main/kotlin/Ejercicio5.kt
+++ src/main/kotlin/Ejercicio5.kt
@@ -2,7 +2,7 @@ fun main(){
     println("¿Cómo es el clima de hoy? ")
     var numero=readLine()!!.toInt() // Declaramos la variable numero para luego utilizarlo en el when
 
-    when(numero){ // Dependiendo del numero que elija, imprimirá lo que aparece abajo
+    when(numero!!){ // Dependiendo del numero que elija, imprimirá lo que aparece abajo, "!!" significa que no puede ser nulo
         1 -> println("Soleado")
         2 -> println("Nublado")
         3 -> println("Lluvioso")
