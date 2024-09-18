9765451868270c6118d3127c28ddc096b1589c95
Óscar Luque Porca <oscarluqueproca@gmail.com>
2024-09-18 16:25:26 +0200

    KOTLIN INTRO COMENTADO

--- src/main/kotlin/Ejercicio5.kt
+++ src/main/kotlin/Ejercicio5.kt
@@ -1,8 +1,8 @@
 fun main(){
     println("¿Cómo es el clima de hoy? ")
-    var numero=readLine()!!.toInt()
+    var numero=readLine()!!.toInt() // Declaramos la variable numero para luego utilizarlo en el when
 
-    when(numero){
+    when(numero){ // Dependiendo del numero que elija, imprimirá lo que aparece abajo
         1 -> println("Soleado")
         2 -> println("Nublado")
         3 -> println("Lluvioso")
--- src/main/kotlin/If.kt
+++ src/main/kotlin/If.kt
@@ -1,7 +1,7 @@
 fun main() {
     print("Introduce la cantidad en miligramos: ")
-    var cantidad = readLine()?.toInt()
-    if (cantidad!! > 100){
+    var cantidad = readLine()?.toInt() // Declaramos la variable cantidad para guardar el dato introducido por el usuario
+    if (cantidad!! > 100){ //Si la cantidad supera los 100 imprimirá un mensaje, si no, imprimirá otro
         println("Felicidades, es una buena poción")
     } else {
         println("La poción es mediocre, sangre sucia inmunda")
@@ -9,9 +9,9 @@ fun main() {
 }
     fun segunda() {
         print("Escribe un número: ")
-        var numero = readLine()?.toInt()
+        var numero = readLine()?.toInt()  //Declaramos ambas variables que nos sirven para guardar el numero del usuario y para alamcenar la sumatoria
         var suma = 0
-        for (i in 1..numero!!) {
+        for (i in 1..numero!!) { // Bucle for en el que mientras que i esté entre el 1 y el número que haya elegido el usuario, almacene esos números en la sumatoria
             suma += i
         }
         println("La suma es $suma")
